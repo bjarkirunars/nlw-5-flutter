d5772417556c4c6b7532a4584433206fd34ffebd
bjarki <bjarki@inecta.com>
2021-04-24 14:55:30 -0300

    Finish day 3

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:DevQuiz/home/home_controller.dart';
+import 'package:DevQuiz/home/home_state.dart';
 import 'package:DevQuiz/home/widgets/appbar/app_bar_widget.dart';
 import 'package:DevQuiz/home/widgets/level_button/level_button_widget.dart';
 import 'package:DevQuiz/home/widgets/quiz_card/quiz_card_widget.dart';
@@ -9,47 +11,75 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
+
+  @override
+  void initState() {
+    super.initState();
+    controller.getUser();
+    controller.getQuizzes();
+    controller.stateNotifier.addListener(() {
+      setState((){});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBarWidget(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                LevelButtonWidget(
-                  label: 'Fácil',
-                ),
-                LevelButtonWidget(
-                  label: 'Médio',
-                ),
-                LevelButtonWidget(
-                  label: 'Difícil',
-                ),
-                LevelButtonWidget(
-                  label: 'Perito',
-                )
-              ],
-            ),
-            SizedBox(
-              height: 24,
-            ),
-            Expanded(child: GridView.count(
-              mainAxisSpacing: 16,
-              crossAxisSpacing: 16,
-              crossAxisCount: 2,
-              children: [
-                QuizCardWidget(),
-                QuizCardWidget(),
-                QuizCardWidget(),
-                QuizCardWidget()
-                ])),
-          ],
+    if (controller.state == HomeState.success) {
+      return Scaffold(
+        appBar: AppBarWidget(
+          user: controller.user!,
         ),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  LevelButtonWidget(
+                    label: 'Fácil',
+                  ),
+                  LevelButtonWidget(
+                    label: 'Médio',
+                  ),
+                  LevelButtonWidget(
+                    label: 'Difícil',
+                  ),
+                  LevelButtonWidget(
+                    label: 'Perito',
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              Expanded(
+                child: GridView.count(
+                    mainAxisSpacing: 16,
+                    crossAxisSpacing: 16,
+                    crossAxisCount: 2,
+                    children: 
+              controller.quizzes!
+                        .map((e) => QuizCardWidget(
+                              title: e.title,
+                              completed:
+                                  "${e.questionsAnswered}/${e.questions.length}",
+                              percent: e.questionsAnswered / e.questions.length,
+                            ))
+                        .toList()
+               ),
+              )
+            ],
+          ),
+        ),
+      );
+    } else {
+      return Scaffold(
+        body: Center(child: CircularProgressIndicator(
+
+        )),
+      );
+    }
   }
 }
--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,10 +1,12 @@
 import 'package:DevQuiz/home/widgets/score_card/score_card_widget.dart';
+import 'package:DevQuiz/shared/models/user_model.dart';
 import 'package:flutter/material.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
 import 'package:DevQuiz/core/app_gradients.dart';
 
 class AppBarWidget extends PreferredSize {
-  AppBarWidget() : super(
+  final UserModel user;
+  AppBarWidget({required this.user}) : super(
     preferredSize: Size.fromHeight(250),
     child: Container(
       height: 250,
@@ -20,7 +22,7 @@ class AppBarWidget extends PreferredSize {
               children: [
                 Text.rich(TextSpan(text: "Olá, ", style: AppTextStyles.title,
                 children: [
-                  TextSpan(text: "Bjarki", style: AppTextStyles.titleBold)
+                  TextSpan(text: user.name, style: AppTextStyles.titleBold)
                 ])),
               Container(
                 width: 58,
@@ -28,7 +30,7 @@ class AppBarWidget extends PreferredSize {
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10),
                   image: DecorationImage(image: NetworkImage(
-                    "https://avatars.githubusercontent.com/u/32462349?s=400&u=0249e09d20c7f8d8c57a8e62f5a6390a987ca948&v=4"))
+                    user.photoUrl))
                 ),)
             ],),
           ),
